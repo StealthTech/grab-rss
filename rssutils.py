934dd867596c253dadb24a56a09a3f336c194d6c
Kitov Marat <mkitov09@gmail.com>
2017-07-18 11:15:01 +0300

    Better links search and normalization

--- rssutils.py
+++ rssutils.py
@@ -17,23 +17,28 @@ def search_rss_meta(html):
     soup = BeautifulSoup(html, 'html.parser')
     meta = soup.find_all('link', {'type': 'application/rss+xml'})
 
-    links = []
-    for link in meta:
-        if link.has_attr('href'):
-            links.append(str(link['href']))
+    rss_links = []
+    for rss_link in meta:
+        if rss_link.has_attr('href'):
+            rss_links.append(str(rss_link['href']))
 
-    return links
+    return rss_links
 
 
 def search_rss_links(html):
     soup = BeautifulSoup(html, 'html.parser')
-    links = []
-    for link in soup.find_all('a'):
-        href = link.get('href')
-        string = link.get_text()
-        if href and ('rss' in href.casefold() or 'rss' in string.casefold()):
-            links.append(href)
-    return links
+    all_links = soup.find_all('a')
+
+    rss_links = []
+    for link in all_links:
+        if link.has_attr('href'):
+            href = link.get('href').casefold()
+            text = link.get_text().casefold()
+            href_match = re.match('([^a-z]|^)rss([^a-z]|$)', href)
+            text_match = re.match('([^a-z]|^)rss([^a-z]|$)', text)
+            if href_match or text_match:
+                rss_links.append(href)
+    return rss_links
 
 
 def search_rss_icons(html):
@@ -62,13 +67,32 @@ def get_content(url):
         return r.text, None
     else:
         return None, error
+        
+
+def traverse_common_links(url):
+    links = [
+        'rss',
+        'rss.xml',
+        'feed'
+    ]
+    
+    if not url.endswith('/'):
+        url += '/'
+        
+    pages_found = []
+    
+    for link in links:
+        r = requests.get(url + link)
+        if r.status_code == 200:
+            pages_found.append(url + link)
+    
+    return pages_found
 
 
 class Entry:
     rss_finders = [
         search_rss_meta,
-        search_rss_links
-        
+        search_rss_links,
     ]
 
     def __init__(self, entry):
@@ -79,6 +103,7 @@ class Entry:
             self.url = 'http://' + self.url
 
         self.html = ''
+        self.title = ''
 
         self.rss = []
         self.request_error = False
@@ -90,9 +115,26 @@ class Entry:
             return None
 
         self.html = html
+        soup = BeautifulSoup(self.html, 'html.parser')
+        self.title = soup.find('title').text
 
         for handler in Entry.rss_finders:
             result = handler(html)
             if result:
                 self.rss = result
                 break
+                
+        self.rss.extend(traverse_common_links(self.url))
+        
+        self._normalize_rss()
+        
+    def _normalize_rss(self):
+        normalized = []
+        for link in self.rss:
+            if not link.casefold().startswith(self.url.casefold()):
+                if link.casefold().startswith('http'): #other website
+                    continue
+                link = self.url + link
+            normalized.append(link.lower())
+            
+        self.rss = list(set(normalized))
